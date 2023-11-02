6457fbc4f145372a3cee7b278a31718c48a00822
payal <payalbenmeda19@gnu.ac.in>
2023-11-03 01:06:02 +0530

    233

--- app/src/main/java/com/example/mad_practical_8/AlarmService.kt
+++ app/src/main/java/com/example/mad_practical_8/AlarmService.kt
@@ -1,28 +1,22 @@
 package com.example.mad_practical_8
-
 import android.app.Service
 import android.content.Intent
-import android.media.AsyncPlayer
 import android.media.MediaPlayer
 import android.os.IBinder
-
 class AlarmService : Service() {
-lateinit var player:MediaPlayer
+    lateinit var player:MediaPlayer
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
-
     override fun onDestroy() {
         player.stop()
         super.onDestroy()
     }
-
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        if(intent!==null)
-        {
+        if (intent != null){
             player = MediaPlayer.create(this,R.raw.alarm)
-            player.stop()
+            player.start()
         }
-        return super.onStartCommand(intent, flags, startId)
+        return START_STICKY
     }
 }
